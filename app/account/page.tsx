a1d2d6fe7395105bc0a5400feb7a792181d30852
w1zl <filin.v2004@gmail.com>
2026-06-19 15:57:19 +0500

    feat: integrate account remote into shell

---
+++ app/account/page.tsx
@@ -0,0 +1,16 @@
+﻿import type { Metadata } from "next";
+
+import { ProfilePageRemote } from "../../components/remotes/account-remotes";
+
+export const metadata: Metadata = {
+  title: "Аккаунт",
+  description: "Профиль покупателя в демонстрационном магазине Shop MFS.",
+  robots: {
+    follow: false,
+    index: false,
+  },
+};
+
+export default function AccountPage() {
+  return <ProfilePageRemote />;
+}
